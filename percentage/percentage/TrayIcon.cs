69d270ade4f1ff4525f0a5dae45603cc3753ecd3
Diego E. Mendoza <86540781+DiegoEli@users.noreply.github.com>
2022-12-19 14:10:39 -0500

    change file
    -se agregó comentarios de posible solución al tamaño necesario para mostrar el texto
    -se añadió cambios en 2 líneas referente a mostrar el mensaje [percentage + "%"] and [" Disconnected"]

--- percentage/percentage/TrayIcon.cs
+++ percentage/percentage/TrayIcon.cs
@@ -38,6 +38,7 @@ namespace percentage
             timer.Start();
         }
 
+        //posible problema al definir el espacio que necesita el Bitmap para mostrar el texto
         private Bitmap GetTextBitmap(String text, Font font, Color fontColor)
         {
             SizeF imageSize = GetStringImageSize(text, font);
@@ -54,7 +55,31 @@ namespace percentage
             }
             return bitmap;
         }
+        /***POSIBLE SOLUCION***
+        private Bitmap GetTextBitmap(String text, Font font, Color fontColor)
+        {
+            // Obtener solo los primeros 4 caracteres de la cadena de texto
+            String truncatedText = text.Substring(0, Math.Min(text.Length, 4));
+        
+            // Calcular el tamaño de la imagen necesaria para la cadena truncada
+            SizeF imageSize = GetStringImageSize(truncatedText, font);
+        
+            Bitmap bitmap = new Bitmap((int)imageSize.Width, (int)imageSize.Height);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.Clear(Color.FromArgb(0, 0, 0, 0));
+                using (Brush brush = new SolidBrush(fontColor))
+                {
+                    graphics.DrawString(truncatedText, font, brush, 0, 0);
+                    graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+                    graphics.Save();
+                }
+            }
+            return bitmap;
+        }
+        */
 
+        //posible problema al definir el espacio que necesita el Bitmap para mostrar el texto
         private static SizeF GetStringImageSize(string text, Font font)
         {
             using (Image image = new Bitmap(1, 1))
@@ -74,7 +99,10 @@ namespace percentage
             PowerStatus powerStatus = SystemInformation.PowerStatus;
             String percentage = (powerStatus.BatteryLifePercent * 100).ToString();
             bool isCharging = SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Online;
-            String bitmapText = isCharging ? percentage + "*" : percentage;
+            //**original-line   |   String bitmapText = isCharging ? percentage + "*" : percentage;
+            String bitmapText = isCharging ? percentage + "%" : percentage + "%";
+            //when printing the text, the space has a small size that does not adapt when adding more than 2 characters: example [60]
+            //it must be adapted to fit 4 characters: example [100%].
             Color bitmapColor;
             RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize");
             if (key != null)
@@ -94,7 +122,8 @@ namespace percentage
                     using (Icon icon = Icon.FromHandle(intPtr))
                     {
                         notifyIcon.Icon = icon;
-                        String toolTipText = percentage + "%" + (isCharging ? " Charging" : "");
+                        //**original-line   |   String toolTipText = percentage + "%" + (isCharging ? " Charging" : "");
+                        String toolTipText = percentage + "%" + (isCharging ? " Charging" : " Disconnected");
                         notifyIcon.Text = toolTipText;
                     }
                 }
