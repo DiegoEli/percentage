a9103f249482dd0f72882fbe1efaf942db2dce29
Diego E. Mendoza Mejía <86540781+DiegoEli@users.noreply.github.com>
2023-09-11 00:43:20 -0500

    Update TrayIcon.cs

--- percentage/percentage/TrayIcon.cs
+++ percentage/percentage/TrayIcon.cs
@@ -1,23 +1,21 @@
+using Microsoft.Win32;
 using System;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using Microsoft.Win32;
 
-namespace percentage 
+namespace percentage_Windows
 {
-    class TrayIcon 
+    class TrayIcon
     {
-        [DllImport("user32.dll", CharSet=CharSet.Auto)]
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
         static extern bool DestroyIcon(IntPtr handle);
 
-        //**original-line   |   private const int fontSize = 18;
-        private const int fontSize = 19;
+        private const int fontSize = 100;
         private const string font = "Segoe UI";
-
         private NotifyIcon notifyIcon;
 
-        public TrayIcon() 
+        public TrayIcon()
         {
             ContextMenu contextMenu = new ContextMenu();
             MenuItem menuItem = new MenuItem();
@@ -39,48 +37,26 @@ namespace percentage
             timer.Start();
         }
 
-        //posible problema al definir el espacio que necesita el Bitmap para mostrar el texto
-        private Bitmap GetTextBitmap(String text, Font font, Color fontColor) 
-        {            
-            SizeF imageSize = GetStringImageSize(text, font);
-            Bitmap bitmap = new Bitmap((int)imageSize.Width, (int)imageSize.Height);
-            using (Graphics graphics = Graphics.FromImage(bitmap)) 
-            {
-                graphics.Clear(Color.FromArgb(0, 0, 0, 0));
-                using (Brush brush = new SolidBrush(fontColor)) 
-                {
-                    graphics.DrawString(text, font, brush, 0, 0);
-                    graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-                    graphics.Save();
-                }
-            }
-            return bitmap;
-        }
-        /***POSIBLE SOLUCION***
         private Bitmap GetTextBitmap(String text, Font font, Color fontColor)
         {
-            // Obtener solo los primeros 4 caracteres de la cadena de texto
-            String truncatedText = text.Substring(0, Math.Min(text.Length, 4));
-        
-            // Calcular el tamaño de la imagen necesaria para la cadena truncada
-            SizeF imageSize = GetStringImageSize(truncatedText, font);
-        
-            Bitmap bitmap = new Bitmap((int)imageSize.Width, (int)imageSize.Height);
+            SizeF imageSize = GetStringImageSize(text, font);
+			Bitmap bitmap = new Bitmap((int)imageSize.Width, (int)imageSize.Height);
+            //Bitmap bitmap = new Bitmap((int)imageSize.Width -100, (int)imageSize.Height);
             using (Graphics graphics = Graphics.FromImage(bitmap))
             {
                 graphics.Clear(Color.FromArgb(0, 0, 0, 0));
+                //graphics.Clear(Color.FromArgb(0, 0, 0));
                 using (Brush brush = new SolidBrush(fontColor))
                 {
-                    graphics.DrawString(truncatedText, font, brush, 0, 0);
+                    graphics.DrawString(text, font, brush, 0, 0);
+                    //graphics.DrawString(text, font, brush, -30, 0);
                     graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
                     graphics.Save();
                 }
             }
             return bitmap;
         }
-        */
 
-        //posible problema al definir el espacio que necesita el Bitmap para mostrar el texto
         private static SizeF GetStringImageSize(string text, Font font)
         {
             using (Image image = new Bitmap(1, 1))
@@ -100,10 +76,10 @@ namespace percentage
             PowerStatus powerStatus = SystemInformation.PowerStatus;
             String percentage = (powerStatus.BatteryLifePercent * 100).ToString();
             bool isCharging = SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Online;
-            //**original-line   |   String bitmapText = isCharging ? percentage + "*" : percentage;
-            String bitmapText = isCharging ? percentage + "%" : percentage + "%";
-            //when printing the text, the space has a small size that does not adapt when adding more than 2 characters: example [60]
-            //it must be adapted to fit 4 characters: example [100%].
+			
+            String bitmapText = isCharging ? percentage + "" : percentage + "";
+			//String bitmapText = isCharging ? percentage + "%" : percentage + "%";
+
             Color bitmapColor;
             RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize");
             if (key != null)
@@ -115,6 +91,7 @@ namespace percentage
             {
                 bitmapColor = Color.White;
             }
+
             using (Bitmap bitmap = new Bitmap(GetTextBitmap(bitmapText, new Font(font, fontSize), bitmapColor)))
             {
                 System.IntPtr intPtr = bitmap.GetHicon();
@@ -123,8 +100,8 @@ namespace percentage
                     using (Icon icon = Icon.FromHandle(intPtr))
                     {
                         notifyIcon.Icon = icon;
-                        //**original-line   |   String toolTipText = percentage + "%" + (isCharging ? " Charging" : "");
-                        String toolTipText = percentage + "%" + (isCharging ? " Charging" : " Disconnected");
+                        //String toolTipText = percentage + "%" + (isCharging ? " Charging" : " Disconnected");
+                        String toolTipText = percentage + "%" + (isCharging ? " Charging AC" : " Not charging");
                         notifyIcon.Text = toolTipText;
                     }
                 }
@@ -133,6 +110,8 @@ namespace percentage
                     DestroyIcon(intPtr);
                 }
             }
+            
         }
+
     }
 }
